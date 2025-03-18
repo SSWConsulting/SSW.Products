3c336a0e2dd101aaacb8c6afad71b9cbe1599045
joshbermanssw <joshberman@ssw.com.au>
2025-03-18 14:17:51 +1100

    start of timeline component

--- components/shared/Blocks/Hero.tsx
+++ components/shared/Blocks/Hero.tsx
@@ -17,22 +17,22 @@ const TranscriptBox = ({ data }: { data: any }) => {
           </div>
           <div className="flex flex-col">
             <span className="font-semibold lg:text-lg text-sm">
-              {data.leftHandSide?.issueReportTitle}
+              {data?.leftHandSide?.issueReportTitle}
             </span>
             <span className="lg:text-sm text-xs">
-              {data.leftHandSide?.issueReportSubTitle}
+              {data?.leftHandSide?.issueReportSubTitle}
             </span>
           </div>
         </div>
 
         <span className="text-red-600 font-mono lg:pt-4 pt-2 lg:text-sm text-xs">
-          {data.leftHandSide?.issueReportByline}
+          {data?.leftHandSide?.issueReportByline}
         </span>
         {/* Transcript Box */}
         <div className="pt-4 font-mono lg:text-sm text-xs">
           <div className="bg-gray-900 flex flex-col p-2 rounded-md border border-gray-600">
             {/* TODO - figure out how to get colour rendering */}
-            <TinaMarkdown content={data.leftHandSide?.issueReportTranscript} />
+            <TinaMarkdown content={data?.leftHandSide?.issueReportTranscript} />
           </div>
         </div>
         {/* Empty Circle Buttons (for now) */}
@@ -60,7 +60,7 @@ const TranscriptBox = ({ data }: { data: any }) => {
               <FaExpandAlt className="hidden group-hover:block absolute text-[8px] text-black" />
             </div>
             <span className="ml-2 lg:text-sm text-xs">
-              {data.rightHandSide?.issueReportSummaryTitle}
+              {data?.rightHandSide?.issueReportSummaryTitle}
             </span>
             <span className="ml-auto text-gray-400 lg:text-sm text-xs">
               v2.4.1
@@ -69,12 +69,12 @@ const TranscriptBox = ({ data }: { data: any }) => {
           {/* Content Box */}
           <div className="rounded-b-lg border-b w-full border-r border-l border-gray-600 bg-gray-900 px-4 py-2 flex flex-col gap-2">
             <span className="pt-4 font-semibold lg:text-sm text-xs">
-              {data.rightHandSide?.issueReportSummarySubtitle}
+              {data?.rightHandSide?.issueReportSummarySubtitle}
             </span>
             <div className="flex flex-col  bg-gray-800 py-3 px-2 rounded-lg gap-2 relative lg:text-sm text-xs">
               {/* Chat tab indicator */}
               <div className="absolute -bottom-2 left-4 w-4 h-2 bg-gray-800 transform -rotate-45 rounded-sm"></div>
-              <TinaMarkdown content={data.rightHandSide.issueReportBody} />
+              <TinaMarkdown content={data?.rightHandSide?.issueReportBody} />
             </div>
             <div className="flex justify-end items-end py-4">
               <div className="bg-red-600 text-xs py-1 px-2 rounded-lg">
@@ -91,7 +91,7 @@ const TranscriptBox = ({ data }: { data: any }) => {
 // This is a work-around for not using markdown. Its a custom 'MDX component' but cant use markdown so we are using a plain string
 // "{ }" indicates text to be highlighted
 const curlyBracketFormatter = (byLine: string) => {
-  return byLine.split(/({.*?})/).map((part, index) =>
+  return byLine?.split(/({.*?})/).map((part, index) =>
     part.startsWith("{") && part.endsWith("}") ? (
       <span
         key={index}
@@ -125,42 +125,42 @@ export default function Hero({ data }: { data: any }) {
       <div className="z-10 flex flex-col items-center justify-center w-full">
         <div className="flex flex-col font-bold items-center justify-center text-2xl md:text-5xl  text-white">
           <div className="pt-20 flex items-center justify-center gap-2">
-            <h1>{data.titleBeforeRotate}</h1>
+            <h1>{data?.titleBeforeRotate}</h1>
             <span className="text-[#CC4141] pl-1">
-              <WordRotate words={data.rotatingWords} className="" />
+              <WordRotate words={data?.rotatingWords} className="" />
             </span>
           </div>
           <div>
-            <h1>{data.titleAfterRotate}</h1>
+            <h1>{data?.titleAfterRotate}</h1>
           </div>
         </div>
         <h2 className="text-white text-center text-base md:text-lg pt-6 lg:pt-12 max-w-3xl px-10 lg:px-0">
-          {curlyBracketFormatter(data.byLine)}
+          {curlyBracketFormatter(data?.byLine)}
         </h2>
 
         {/* Buttons */}
         <div className="flex items-center justify-center pt-12 gap-6">
-          {data.ctaLeft?.title && data.ctaLeft?.link && (
+          {data?.ctaLeft?.title && data?.ctaLeft?.link && (
             <div>
               <ShinyButton className="bg-gradient-to-br from-rose-300 to-[#CC4141] text-white py-4 px-6 border border-white/30 hover:-top-1 transition-all ease-in-out duration-300 relative top-0">
-                {data.ctaLeft?.title}
+                {data?.ctaLeft?.title}
               </ShinyButton>
             </div>
           )}
-          {data.ctaRight?.title && data.ctaRight?.link && (
+          {data?.ctaRight?.title && data?.ctaRight?.link && (
             <div>
               <ShinyButton className="bg-[#131313] text-white py-4 px-6 border border-white/30 hover:-top-1 transition-all ease-in-out duration-300 relative top-0">
-                {data.ctaRight?.title}
+                {data?.ctaRight?.title}
               </ShinyButton>
             </div>
           )}
         </div>
         <span className="flex justify-center text-white text-center lg:text-sm text-xs pt-4">
-          {data.buttonSubtext}
+          {data?.buttonSubtext}
         </span>
         <div className="flex items-center justify-center pt-12 text-white max-w-6xl w-full">
           {/* Transcript Container */}
-          <TranscriptBox data={data.reportUI} />
+          <TranscriptBox data={data?.reportUI} />
         </div>
       </div>
     </div>
