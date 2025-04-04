3abbf961489c5662cd0d14fae804651f17963970
Calinator444 <calinator666@gmail.com>
2025-04-04 12:00:53 +1100

    added privacy policy page

---
+++ tina/collectionSchema/privacy.tsx
@@ -0,0 +1,50 @@
+import { Collection, TinaField } from "tinacms";
+import { seoInformation } from "../shared/SEOInformation";
+
+export const privacyPolicyCollection: Collection = {
+  label: "Privacy Policy",
+  ui: {
+    allowedActions: {
+        create: false,
+        delete: false,
+    },
+  },
+  name: "privacy",
+  path: "content/pages/",
+  format: "mdx",
+  fields: [
+    seoInformation as TinaField,
+    {
+      type: "string",
+      name: "title",
+      label: "Title",
+      required: true,
+      list: false,
+      ui: {
+        defaultValue: "Privacy Policy",
+        validate: (value) => {
+          if (value?.length > 70) {
+            return "Title can not be more then 70 characters long";
+          }
+        },
+      },
+    },
+    {
+      type: "string",
+      name: "date",
+      label: "Date Created",
+      ui: {
+        component: "date",
+      },
+    },
+    {
+      type: "rich-text",
+      name: "body",
+      label: "Body",
+      isBody: true,
+      parser: {
+        type: "mdx",
+      },
+    },
+  ],
+};
