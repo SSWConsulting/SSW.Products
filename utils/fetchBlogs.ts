0f95e3cdddab09f986f50dff0c1fb198e98510ae
Calinator444 <calinator666@gmail.com>
2025-04-09 14:22:16 +1000

    added case insensitive fuzzy searching

--- utils/fetchBlogs.ts
+++ utils/fetchBlogs.ts
@@ -32,10 +32,13 @@ export async function getBlogsForProduct({
 }: GetBlogsForProductProps) {
   try {
     const titles = await getArticleTitles();
-
     const titleFilter = keyword
       ? {
-          title: { in: titles.filter((title) => title.includes(keyword)) },
+          title: {
+            in: titles.filter((title) =>
+              title.toLowerCase().includes(keyword.toLowerCase())
+            ),
+          },
         }
       : {};
 
@@ -72,29 +75,6 @@ export async function getBlogsForProduct({
     );
 
     return res.data.blogsConnection;
-
-    // if (!filteredBlogs || !filteredBlogs.length) {
-    //   throw new Error("No documents found");
-    // }
-
-    // const sortedBlogs = filteredBlogs.sort((a ,b) => {
-    //   if(!a?.node?.date || !b?.node?.date) return 0;
-    //   const dateA = new Date(a.node.date);
-    //   const dateB = new Date(b.node.date);
-    //   return dateB.getTime() - dateA.getTime();
-    // });
-
-    // const paginatedBlogs = sortedBlogs.slice(offset, offset + limit);
-
-    // {
-    //   // query: res.query,
-    //   return
-    //   data: filteredBlogs.map((edge) => {
-    //     if(!edge?.node) return null;
-    //       return edge.node
-    //   }),
-    //   // hasMore: filteredBlogs.length > offset + limit,
-    // };
   } catch (error) {
     console.error("Error fetching TinaCMS blog data:", error);
     throw error;
