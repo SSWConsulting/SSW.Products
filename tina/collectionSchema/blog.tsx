f8bed95c98d3079c2c6fe83747f9e204ad7b4d34
Calinator444 <calinator666@gmail.com>
2025-04-15 16:23:56 +1000

    cleaned up legacy CTA collection

--- tina/collectionSchema/blog.tsx
+++ tina/collectionSchema/blog.tsx
@@ -133,45 +133,6 @@ const heroSearchTemplate: Template = {
   ],
 };
 
-const newsletterTemplate: Template = {
-  label: "Call To Action",
-  name: "callToAction",
-  ui: {
-    defaultItem: () => {
-      return { title: "Title" };
-    },
-  },
-  fields: [
-    {
-      name: "title",
-      label: "Title",
-      type: "string",
-    },
-    {
-      name: "description",
-      label: "Description",
-      type: "string",
-    },
-    {
-      name: "button",
-      type: "object",
-      label: "Button",
-      fields: [
-        {
-          name: "buttonText",
-          label: "Button Text",
-          type: "string",
-        },
-        {
-          name: "buttonLink",
-          label: "Button Text",
-          type: "string",
-        },
-      ],
-    },
-  ],
-};
-
 const featuredBlogTemplate: Template = {
   label: "Featured Blog",
   name: "featuredBlog",
