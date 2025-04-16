3ec31470e9076b5dcb6a943b62350a85377535c8
joshbermanssw <joshberman@ssw.com.au>
2025-04-16 12:28:54 +1000

    rm log and refresh lock

--- components/shared/blocksRenderer.tsx
+++ components/shared/blocksRenderer.tsx
@@ -13,16 +13,14 @@ import {
   ImageTextBlock,
   LogoCarousel,
 } from "ssw-tinacms-landingkit";
-import * as AntIcons from "react-icons/ai";
-import Hero from "./Blocks/Hero/Hero";
-import { Timeline } from "./Blocks/Timeline/Timeline";
+
 import Container from "../Container";
 import BentoBox from "./Blocks/BentoBox/BentoBox";
 import CalculatorComponent from "./Blocks/Calculator";
 import CallToAction from "./Blocks/CallToAction";
 import CardAndImageParent from "./Blocks/CardAndImage/CardAndImage";
 import ComparisonTable from "./Blocks/ComparisonTable";
-import Hero from "./Blocks/Hero";
+import Hero from "./Blocks/Hero/Hero";
 import { Timeline } from "./Blocks/Timeline/Timeline";
 
 interface Block {
