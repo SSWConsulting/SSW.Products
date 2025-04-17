d45d96686279ae05d7070455f3baaeb3efa17dc7
Calinator444 <calinator666@gmail.com>
2025-04-17 12:03:18 +1000

    📱 Fixed gradient on mobile

--- components/shared/Blocks/Hero/GradientBackground.tsx
+++ components/shared/Blocks/Hero/GradientBackground.tsx
@@ -2,7 +2,7 @@ export function GradientBackground() {
   return (
     <div className="absolute inset-0 z-[-1] pointer-events-none">
       <div
-        className="absolute w-[100%] h-[50rem] blur-[150px] opacity-[59%] rounded-full left-[-50%] top-[30%] z-[-1] mx-auto"
+        className="absolute w-[100%] h-[50rem] blur-[150px] opacity-[59%] rounded-full left-[-50%] top-[12%] sm:top-[30%] z-[-1] mx-auto"
         style={{
           background:
             "linear-gradient(to bottom right, #FF9D3F 0%, #F46772 40%, #AF33E4 69%, black 100%)", //in-line style is the best way to get control of a 40 stop gradient
