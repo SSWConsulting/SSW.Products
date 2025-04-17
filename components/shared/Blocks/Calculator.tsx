531d3189f762724c5fe10a6e1397b8642e73c1e8
Calinator444 <calinator666@gmail.com>
2025-04-17 17:43:07 +1000

    fixed Calculator slider not being under handle

--- components/shared/Blocks/Calculator.tsx
+++ components/shared/Blocks/Calculator.tsx
@@ -40,7 +40,7 @@ const CustomSlider = ({
   };
 
   const percentage =
-    ((value - initialValue) / (finalValue - initialValue)) * 100;
+    ((value - initialValue) / (finalValue - initialValue)) * 100 + 0.05;
 
   const halfWayValue = (initialValue + finalValue) / 2;
 
