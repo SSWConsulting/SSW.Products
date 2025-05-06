e49f537fd2e03e4558e966c03a61561b7d6dfe92
Steven Qiang [SSW] <doublestrong@hotmail.com>
2025-05-06 12:19:37 +1000

    Update components/shared/Blocks/BentoBox/BentoBox.tsx

--- components/shared/Blocks/BentoBox/BentoBox.tsx
+++ components/shared/Blocks/BentoBox/BentoBox.tsx
@@ -271,7 +271,7 @@ export default function BentoBox({ data }: { data: any }) {
                   <h2 className="text-white text-2xl font-semibold">
                     {topRightBox.title}
                   </h2>
-                  <p className="text-[#797979] text-s">
+                  <p className="text-[#797979] text-sm">
                     {topRightBox.description}
                   </p>
                 </div>
