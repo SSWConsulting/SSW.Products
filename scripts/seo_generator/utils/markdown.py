41e8702c3dbec8f647e920287fd1557e4121ffc6
Calinator444 <calinator666@gmail.com>
2025-06-10 09:43:56 +1000

    removed seo generator script from this branch

--- scripts/seo_generator/utils/markdown.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-def write_mdx_file(file_path: str, frontmatter: str, body: str):
-    with open(file_path, 'w', encoding='utf-8') as f:
-        f.write('---\n')
-        yaml.dump(frontmatter, f)
-        f.write('---\n')
-        if(body):
-            f.write(body)
--- scripts/seo_generator/utils/urls.py
+++
@@ -1,8 +0,0 @@
-def path_to_url(path:str, website_url: str):
-   path = path[2:]
-   path = path.replace("\\", "/")
-   path = path.replace("index.mdx", "")
-   path = path.replace(".mdx", "")
-   path = path.replace(".json", "")
-   path = path.replace("pages/", "")
-   return website_url + path
