b1b2b8377dfc159131c0ed4520bc11f022bc1b1b
Calinator444 <calinator666@gmail.com>
2025-06-11 08:56:38 +1000

    Fixed responsive view for 404 page & wired data from Tina

--- app/[product]/not-found.tsx
+++ app/[product]/not-found.tsx
@@ -1,31 +1,29 @@
-import SadYak from "@images/sad-yak.png";
-import Image from "next/image";
-export default function NotFound() {
-  return (
-    <div className="flex items-center">
-      <div className="grid h-fit grid-cols-2 w-248 mx-auto">
-        <Image
-          aria-hidden={true}
-          className="size-109 rounded-2xl"
-          quality={90}
-          alt=""
-          src={SadYak}
-        />
-        {/* background: linear-gradient(98.94deg, #CC4141 48.51%, #D699FB 100.38%, #FF778E 178.58%);
-         */}
-        <div className="flex-col flex justify-center text-white">
-          <h1 className="text-6xl  w-fit text-transparent from-50% font-bold via-100% to-180% bg-linear-100 from-ssw-red via-[#D699FB] to-[#FF778E] bg-clip-text">
-            404
-          </h1>
-          <p className="text-xl text-white">
-            This page isn’t in our training data. Even our Yak couldn’t generate
-            a prediction for it. If you got here through a broken link consider
-            YakShaving it!
-          </p>
-          <p></p>
-        </div>
-      </div>
-      {/* </Container> */}
-    </div>
+import { Product } from "@/types/product-list";
+import client from "@tina/__generated__/client";
+import NotFoundClient from "./not-found/not-found-client";
+export default async function NotFound() {
+  const products = process.env.NEXT_PUBLIC_PRODUCT_LIST;
+  if (!products) {
+    throw Error(
+      "NEXT_PUBLIC_PRODUCT_LIST is not defined in the environment variables."
+    );
+  }
+  const productList = JSON.parse(products) as Product[]; // Ensure this is used somewhere or remove it if not needed
+
+  const productStrings = productList.map((product) => product.product);
+
+  const productQueries = await Promise.all(
+    productList.map((product) => {
+      return client.queries.notFound({
+        relativePath: `${product.product}/index.mdx`,
+      });
+    })
   );
+
+  const productDictionary = productStrings.reduce((acc, product, index) => {
+    acc[product] = productQueries[index];
+    return acc;
+  }, {} as Record<string, (typeof productQueries)[number]>);
+
+  return <NotFoundClient notFoundDictionary={productDictionary} />;
 }
---
+++ app/[product]/not-found/not-found-client.tsx
@@ -0,0 +1,70 @@
+"use client";
+import client from "@tina/__generated__/client";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { tinaField, useTina } from "tinacms/dist/react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+type NotFound = Awaited<ReturnType<typeof client.queries.notFound>>;
+
+type NotFoundData = Record<string, NotFound>;
+
+const NotFoundClient = ({
+  notFoundDictionary,
+}: {
+  notFoundDictionary: NotFoundData;
+}) => {
+  console.log("notFoundDictionary", notFoundDictionary);
+  const params = useParams<{ product: string }>();
+  console.log("params", params.product);
+  const tinaData = notFoundDictionary[params.product];
+  const { data } = useTina({
+    query: tinaData.query,
+    variables: tinaData.variables,
+    data: tinaData.data,
+  });
+  const { imgHeight, imgSrc, imgWidth, body, heading } = data.notFound;
+
+  return (
+    <div className="flex items-center">
+      <div className="grid px-4 pb-12 lg:px-0 grid-cols-1 gap-10 h-fit md:grid-cols-2 w-248 mx-auto">
+        {imgSrc && imgHeight && imgWidth && (
+          <Image
+            data-tina-field={tinaField(data.notFound, "imgSrc")}
+            aria-hidden={true}
+            className="sm:w-109 md:ml-0 mx-auto w-60 rounded-2xl"
+            quality={90}
+            width={imgWidth}
+            height={imgHeight}
+            alt=""
+            src={imgSrc}
+          />
+        )}
+        {/* background: linear-gradient(98.94deg, #CC4141 48.51%, #D699FB 100.38%, #FF778E 178.58%);
+         */}
+        <div className="flex-col flex justify-center text-white">
+          <h1
+            data-tina-field={tinaField(data.notFound, "heading")}
+            className="text-6xl  w-fit text-transparent from-50% font-bold via-100% to-180% bg-linear-100 from-ssw-red via-[#D699FB] to-[#FF778E] bg-clip-text"
+          >
+            {heading}
+          </h1>
+          <section
+            className="[&_a]:underline [&_a]:underline-offset-2"
+            data-tina-field={tinaField(data.notFound, "body")}
+          >
+            <TinaMarkdown content={body} />
+          </section>
+          {/* <p className="text-xl text-white">
+          This page isn’t in our training data. Even our Yak couldn’t generate a
+          prediction for it. If you got here through a broken link consider
+          YakShaving it!
+        </p> */}
+        </div>
+      </div>
+      {/* </Container> */}
+    </div>
+  );
+};
+
+export default NotFoundClient;
+// This component is a placeholder for the Not Found page in a Next.js application
