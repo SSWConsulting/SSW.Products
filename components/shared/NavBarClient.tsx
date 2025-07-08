74155b96162d188bb7152608760b83cdd5c7c779
Calinator444 <calinator666@gmail.com>
2025-07-08 17:25:25 +1000

    fixed additional responsiveness issues

--- components/shared/NavBarClient.tsx
+++ components/shared/NavBarClient.tsx
@@ -14,6 +14,7 @@ import {
 
 import { Button } from "@comps/ui/button";
 import clsx from "clsx";
+import React from "react";
 import { FaChevronRight, FaExternalLinkAlt } from "react-icons/fa";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import { NavigationBarQuery } from "../../tina/__generated__/types";
@@ -39,12 +40,16 @@ export default function NavBarClient({ results }: NavBarClientProps) {
   });
   console.log("NavBarClient rendered", results);
   const { navigationBar } = results || {};
-  const leftNavItems =
-    navigationBar?.leftNavItem?.filter((item) => item !== null) || [];
+  const leftNavItems = React.useMemo(() => {
+    return navigationBar?.leftNavItem?.filter((item) => item !== null) || [];
+  }, [navigationBar?.leftNavItem]);
+
   const buttons =
     navigationBar?.buttons?.filter((button) => button !== null) || [];
   // const rightNavItems = navigationBar?.rightNavItem;
   const { imgSrc, imgHeight, imgWidth } = navigationBar || {};
+
+  console.log("filtering");
 
   // MenuItem component for single navigation items
 
@@ -83,14 +88,14 @@ export default function NavBarClient({ results }: NavBarClientProps) {
 
   return (
     <NavigationMenu.Root
-      className={`text-white relative transition-colors justify-center w-screen z-10  duration-300 ease-in-out ${
+      className={`text-white sticky @container transition-colors justify-center z-10  duration-300 ease-in-out ${
         scrolled
           ? `shadow-xs bg-[#131313]/80 my-2 py-4 animate-slide animate-in slide-in-from-top-3 backdrop-blur-sm animate-slide-in top-0 `
           : "py-6"
       } z-40 w-full`}
     >
-      <NavigationMenu.List className="flex mx-auto max-w-7xl m-0 justify-center">
-        <NavigationMenu.Item className="gap-8 mx-auto flex @container items-center w-full">
+      <NavigationMenu.List className="flex  mx-4 xl:mx-auto max-w-7xl m-0 justify-center">
+        <NavigationMenu.Item className="gap-8  mx-auto flex @container items-center w-full">
           {imgWidth && imgHeight && imgSrc && (
             <Link className="mb-2 shrink-0" href="/">
               <Image
@@ -111,12 +116,15 @@ export default function NavBarClient({ results }: NavBarClientProps) {
             item.items.length > 0
           ) {
             return (
-              <NavigationMenu.Item key={index}>
-                <NavigationMenu.Trigger className="outline-none  flex items-center gap-2 px-3  rounded hover:bg-white/10 transition-colors">
+              <NavigationMenu.Item
+                className="my-auto hidden @7xl:block"
+                key={index}
+              >
+                <NavigationMenu.Trigger className="outline-none text-base h-fit flex items-center gap-2 px-3  rounded  transition-colors">
                   {item.label}
                   <FaChevronRight className="text-red-500 text-sm rotate-90 transition-all duration-300" />
                 </NavigationMenu.Trigger>
-                <NavigationMenu.Content className="border-0 absolute data-[state=open]:animate-in  data-[state=closed]:animate-out animation-duration-100 data-[state=open]:fade-in data-[state=closed]:fade-out">
+                <NavigationMenu.Content className="border rounded text-[#d1d5db] hover:text-white border-white/20 shadow-lg p-3 space-y-2 bg-gray-light absolute data-[state=open]:animation-duration-100 data-[state=open]:animate-in  data-[state=closed]:animate-out data-[state=closed]:animation-duration-300 data-[state=open]:fade-in data-[state=closed]:fade-out">
                   {/* <ul className="space-y-2"> */}
                   {item.items
                     .filter(
@@ -147,10 +155,13 @@ export default function NavBarClient({ results }: NavBarClientProps) {
             item.label
           ) {
             return (
-              <NavigationMenu.Item key={index}>
+              <NavigationMenu.Item
+                className="my-auto hidden @7xl:block"
+                key={index}
+              >
                 <Link
                   href={item.href}
-                  className="px-3 py-2 rounded hover:bg-white/10 transition-colors uppercase"
+                  className="px-3 hover:decoration-ssw-red decoration-transparent underline-offset-3 underline text-base block h-fit rounded transition-colors uppercase"
                 >
                   {item.label}
                 </Link>
@@ -162,7 +173,7 @@ export default function NavBarClient({ results }: NavBarClientProps) {
 
         {buttons.map((button, index) => {
           return (
-            <NavigationMenu.Item key={index}>
+            <NavigationMenu.Item className="pl-12 last:pl-8" key={index}>
               <ButtonMap item={button} />
             </NavigationMenu.Item>
           );
@@ -298,7 +309,7 @@ const SubMenuItem = ({
     <li className="hidden xl:flex items-center group relative">
       <span className="cursor-pointer flex items-center gap-2">
         {label.toUpperCase()}{" "}
-        <FaChevronRight className="text-red-500 text-sm rotate-90 transition-all duration-300" />
+        <FaChevronRight className="text-red-500 text-m rotate-90 transition-all duration-300" />
       </span>
       <div className="absolute top-full left-0 opacity-0 invisible group-hover:opacity-100 group-hover:visible pt-2 transition-all duration-300">
         <ul className="bg-[#222222] text-[#D1D5DB] border border-white/20 mt-0 space-y-2 p-3 rounded shadow-lg min-w-[150px] z-10">
