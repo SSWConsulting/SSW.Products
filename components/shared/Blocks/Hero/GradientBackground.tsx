274a0cf719f34e9ad1a2316a1b2aa61c47597049
Calinator444 <calinator666@gmail.com>
2025-08-01 12:56:57 +1000

    moved gradient instead of using svg code

--- components/shared/Blocks/Hero/GradientBackground.tsx
+++ components/shared/Blocks/Hero/GradientBackground.tsx
@@ -1,60 +1,15 @@
-export function GradientBackground({ className }: { className?: string }) {
+export function GradientBackground() {
   return (
-    <svg
-      className={className}
-      width="2074"
-      height="1585"
-      viewBox="0 0 2074 1585"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g filter="url(#filter0_f_1844_3507)">
-        <path
-          d="M1714 792.5C1714 1031.36 1326.39 1225 848.247 1225C370.104 1225 340 1031.36 340 792.5C340 553.637 370.104 360 848.247 360C1326.39 360 1714 553.637 1714 792.5Z"
-          fill="url(#paint0_linear_1844_3507)"
-        />
-        <path
-          d="M1714 792.5C1714 1031.36 1326.39 1225 848.247 1225C370.104 1225 340 1031.36 340 792.5C340 553.637 370.104 360 848.247 360C1326.39 360 1714 553.637 1714 792.5Z"
-          fill="black"
-          fill-opacity="0.4"
-        />
-      </g>
-      <defs>
-        <filter
-          id="filter0_f_1844_3507"
-          x="-20"
-          y="0"
-          width="2094"
-          height="1585"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
-        >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            result="shape"
-          />
-          <feGaussianBlur
-            stdDeviation="180"
-            result="effect1_foregroundBlur_1844_3507"
-          />
-        </filter>
-        <linearGradient
-          id="paint0_linear_1844_3507"
-          x1="-17.5067"
-          y1="362.248"
-          x2="1529.13"
-          y2="1492.28"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#FF9D3F" />
-          <stop offset="0.403862" stop-color="#F46772" />
-          <stop offset="0.692179" stop-color="#AF33E4" />
-          <stop offset="1" stop-color="#080808" />
-        </linearGradient>
-      </defs>
-    </svg>
+    <div className="absolute inset-0 z-[-1] pointer-events-none">
+      {/*Webkit Transform fixes for IOS*/}
+      <div
+        className="absolute w-full h-200 blur-[150px] opacity-59 rounded-full -left-[80%] top-[12%] sm:top-[30%] z-[-1] mx-auto"
+        style={{
+          WebkitTransform: "translate3d(0, 0, 0);",
+          background:
+            "linear-gradient(to bottom right, #FF9D3F 0%, #F46772 40%, #AF33E4 69%, black 100%)", //in-line style is the best way to get control of a 40 stop gradient
+        }}
+      />
+    </div>
   );
 }
