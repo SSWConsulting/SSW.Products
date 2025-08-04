56dd3736cc75d26b7d7ac50c22fa30164c7a4ba1
Tiago Araújo [SSW] <tiagov8@gmail.com>
2025-08-04 10:49:35 -0700

    Update layout.tsx to apply to desktop/tablet only

--- app/[product]/docs/layout.tsx
+++ app/[product]/docs/layout.tsx
@@ -18,14 +18,14 @@ const RootLayout = async ({
       <SearchBox.Root index={tableOfContentsData.algoliaSearchIndex!}>
         <div className="grid grid-cols-1 h-full  md:grid-cols-[1.25fr_3fr] lg:grid-cols-[1fr_3fr] max-w-360 mx-auto">
           {/* LEFT COLUMN 1/3 */}
-          <div className=" max-h-[calc(100vh-13rem)] mt-20 hidden md:block py-8 bg-gray-darkest max-w-[calc(100%_-_2rem)]   mb-8 rounded-lg left-4 top-44 text-white self-start px-6  overflow-y-auto [scrollbar-width:thin] [scrollbar-color:var(--color-ssw-charcoal)_transparent] sticky">
+          <div className=" max-h-[calc(100vh-13rem)] mt-20 hidden md:block py-8 bg-gray-darkest max-w-[calc(100%_-_2rem)] mb-8 rounded-lg left-4 top-44 text-white self-start px-6 overflow-y-auto [scrollbar-width:thin] [scrollbar-color:var(--color-ssw-charcoal)_transparent] sticky">
             <TableOfContentsClient
               tableOfContentsData={tableOfContentsData as DocsTableOfContents}
             />
           </div>
 
           {/* RIGHT COLUMN 2/3 */}
-          <div className="grow pl-4 pr-12 sm:pt-20 sm:pt-20 ">{children}</div>
+          <div className="grow pl-4 pr-4 md:pr-12 pt-0 md:pt-20">{children}</div>
         </div>
       </SearchBox.Root>
     </>
