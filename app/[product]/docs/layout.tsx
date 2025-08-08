cf8f657ac4a4a8b35122a89375cb801116325aa3
ZenoWang1999 <ZenoWang1999@gmail.com>
2025-08-08 16:12:14 +0800

    fix error

--- app/[product]/docs/layout.tsx
+++ app/[product]/docs/layout.tsx
@@ -17,7 +17,7 @@ const RootLayout = async ({
   const tableOfContentsData = await getDocsTableOfContents(params.product, locale);
   return (
     <>
-      <SearchBox.Root index={tableOfContentsData?.algoliaSearchIndex!}>
+      <SearchBox.Root index={tableOfContentsData?.algoliaSearchIndex || ""}>
         <div className="grid grid-cols-1 h-full  md:grid-cols-[1.25fr_3fr] lg:grid-cols-[1fr_3fr] max-w-360 mx-auto">
           {/* LEFT COLUMN 1/3 */}
           <div className=" max-h-[calc(100vh-13rem)] mt-20 hidden md:block py-8 bg-gray-darkest max-w-[calc(100%_-_2rem)]   mb-8 rounded-lg left-4 top-44 text-white self-start px-6  overflow-y-auto [scrollbar-width:thin] [scrollbar-color:var(--color-ssw-charcoal)_transparent] sticky">
