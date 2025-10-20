6b102cce958a5ff194b9befbfa06b72a19e8c63f
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-10-20 12:17:27 +1100

    Using GTM tag from next

--- app/[product]/layout.tsx
+++ app/[product]/layout.tsx
@@ -5,12 +5,12 @@ import NavBarServer from "../../components/shared/NavBarServer";
 import { getGoogleTagId } from "../../utils/getGoogleTagId";
 import { getLocale } from "../../utils/i18n";
 import "../globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({
   weight: ["400", "600", "700"],
   subsets: ["latin"],
 });
-
 
 export default function RootLayout({
   children,
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   const googleTagId = getGoogleTagId(params.product);
   const locale = getLocale();
-  const htmlLang = locale === 'zh' ? 'zh-CN' : 'en';
+  const htmlLang = locale === "zh" ? "zh-CN" : "en";
 
   return (
     <html lang={htmlLang}>
@@ -34,15 +34,7 @@ export default function RootLayout({
             src="https://plausible.io/js/script.hash.outbound-links.pageview-props.tagged-events.js"
           />
         )}
-        {googleTagId && (
-          <Script id="google-tag-manager" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${googleTagId}');`}
-          </Script>
-        )}
+        {googleTagId && <GoogleTagManager gtmId={googleTagId} />}
       </head>
       <body
         className={`min-h-screen flex-col flex ${inter.className} relative bg-gray-light`}
