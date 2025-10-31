f1589dd7804ab17e069019d14eb91b9115105675
Calinator444 <calebwilliams@ssw.com.au>
2025-10-31 13:32:39 +1100

    Fixed code indenting

--- app/[product]/feedback/page.tsx
+++ app/[product]/feedback/page.tsx
@@ -23,11 +23,11 @@ export default async function Feedback({ params }: Feedback) {
         notFound();
 
   return <>
-  <div
-        data-form-id='abcf6de1-3cae-f011-bbd3-000d3ad0fcef'
-        data-form-api-url='https://public-oce.mkt.dynamics.com/api/v1.0/orgs/21dfd5be-b4a6-4cf1-ab3c-316c54b7089a/landingpageforms'
-        data-cached-form-url='https://assets-oce.mkt.dynamics.com/21dfd5be-b4a6-4cf1-ab3c-316c54b7089a/digitalassets/forms/abcf6de1-3cae-f011-bbd3-000d3ad0fcef' >
-        </div>
-        <Script src='https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/oce/FormLoader/FormLoader.bundle.js' />
+            <div
+            data-form-id='abcf6de1-3cae-f011-bbd3-000d3ad0fcef'
+            data-form-api-url='https://public-oce.mkt.dynamics.com/api/v1.0/orgs/21dfd5be-b4a6-4cf1-ab3c-316c54b7089a/landingpageforms'
+            data-cached-form-url='https://assets-oce.mkt.dynamics.com/21dfd5be-b4a6-4cf1-ab3c-316c54b7089a/digitalassets/forms/abcf6de1-3cae-f011-bbd3-000d3ad0fcef' >
+            </div>
+            <Script src='https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/oce/FormLoader/FormLoader.bundle.js' />
         </>
 }
