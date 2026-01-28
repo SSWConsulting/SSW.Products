1e84203937b736a00a41dfa6cc6fb673145f3522
Calinator444 <calebwilliams@ssw.com.au>
2026-01-28 16:20:50 +1100

    added timepro as a valid domain for cross origin requests

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["assets.tina.io", "img.youtube.com"],
+    domains: ["assets.tina.io", "img.youtube.com", "sswtimepro.com"],
   },
   async redirects() {
     return [
