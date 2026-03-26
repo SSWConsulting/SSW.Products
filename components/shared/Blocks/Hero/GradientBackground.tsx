cca8984198fa3e14c2db3858c5557b762ca9261c
Willow Lyu <iqay_lyu@hotmail.com>
2026-03-26 16:01:06 +0800

    fix errors and update image

--- components/shared/Blocks/Hero/GradientBackground.tsx
+++ components/shared/Blocks/Hero/GradientBackground.tsx
@@ -5,7 +5,7 @@ export function GradientBackground() {
       <div
         className="absolute w-full h-200 blur-[150px] opacity-59 rounded-full -left-[80%] top-[12%] sm:top-[30%] z-[-1] mx-auto"
         style={{
-          WebkitTransform: "translate3d(0, 0, 0);",
+          WebkitTransform: "translate3d(0, 0, 0)",
           background:
             "linear-gradient(to bottom right, #FF9D3F 0%, #F46772 40%, #AF33E4 69%, black 100%)", //in-line style is the best way to get control of a 40 stop gradient
         }}
